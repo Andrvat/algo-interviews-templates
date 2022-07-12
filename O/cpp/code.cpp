e70c238a0c968051282c134a5fd1f38e3f330573
andrvat <andreivalitov2001@gmail.com>
2022-07-13 00:15:48 +0700

    TL on 23 test on task O

--- O/cpp/code.cpp
+++ O/cpp/code.cpp
@@ -1,23 +1,59 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+/*
+Объяснение идеи (23 тест TL, асимтотика O(N^2), нужна н лог н), см. по ссылке
+https://vk.com/im?act=browse_images&id=1325903
+Если ссылка не рабочая, то ищи две фотографии с листочками-стикерами в Favorites.
+*/
 
 struct Building {
     int needCapital;
     int addedCapital;
 
-    Building(int c, int p) {
+    Buildinаg(int c, int p) {
         needCapital = c;
         addedCapital = p;
+    }
+
+    friend bool operator<(const Building &l, const Building &r) {
+        if (l.needCapital == r.needCapital) {
+            return l.addedCapital < r.addedCapital;
+        }
+        return l.needCapital < r.needCapital;
     }
 };
 
 
-long long getMaxFinalCapital(const vector<Building>& buildings, int startCapital, int maxNumberOfBuildings) {
-    // your code goes here
-    return 0;
+long long getMaxFinalCapital(const vector<Building> &buildings, int startCapital, int maxNumberOfBuildings) {
+    std::vector<int64_t> previousRow(maxNumberOfBuildings + 1, startCapital);
+    std::vector<int64_t> currentRow(maxNumberOfBuildings + 1);
+    std::vector<Building> sortedBuildings = buildings;
+    std::sort(sortedBuildings.begin(), sortedBuildings.end());
+    for (int32_t availableFirst = 1; availableFirst <= sortedBuildings.size(); ++availableFirst) {
+        currentRow[0] = startCapital;
+        for (int32_t takenBuildings = 1; takenBuildings <= maxNumberOfBuildings; ++takenBuildings) {
+            if (takenBuildings > availableFirst) {
+                break;
+            }
+            int64_t optionalSum = -1;
+            if (previousRow[takenBuildings - 1] >= sortedBuildings[availableFirst - 1].needCapital) {
+                optionalSum = sortedBuildings[availableFirst - 1].addedCapital + previousRow[takenBuildings - 1];
+            }
+            int64_t optionalValue = -1;
+            if (takenBuildings < availableFirst) {
+                optionalValue = previousRow[takenBuildings];
+            }
+            currentRow[takenBuildings] = std::max(std::max(optionalValue,
+                                                           currentRow[takenBuildings - 1]),
+                                                  optionalSum);
+        }
+        previousRow = currentRow;
+    }
+    return currentRow[maxNumberOfBuildings];
 }
 
 
@@ -27,19 +63,20 @@ vector<Building> readBuildings(int n) {
         int c, p;
         cin >> c >> p;
         buildings.push_back(Building(c, p));
-    } 
+    }
     return buildings;
 }
 
 int main() {
-        int n;
-        cin >> n;
-        int k;
-        cin >> k;
-        vector<Building> buildings = readBuildings(n);
-        int M;
-        cin >> M;
-        cout << getMaxFinalCapital(buildings, M, k) << endl;
+    int n;
+    cin >> n;
+    int k;
+    cin >> k;
+    vector<Building> buildings = readBuildings(n);
+    int M;
+    cin >> M;
+    cout << getMaxFinalCapital(buildings, M, k) << endl;
+    return 0;
 }
 
 
